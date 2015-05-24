d50f2baf9f02f46a168a5b660bde4cd4ce784c32
souryavarenya <souryavarenya@gmail.com>
2015-05-25 03:06:59 +0530

    Create facematch.py

---
+++ facematch.py
@@ -0,0 +1,46 @@
+API_KEY = '*********************'
+API_SECRET = '********************'
+
+#initialisation
+import time
+from pprint import pformat
+def print_result(hint, result):
+    def encode(obj):
+        if type(obj) is unicode:
+            return obj.encode('utf-8')
+        if type(obj) is dict:
+            return {encode(k): encode(v) for (k, v) in obj.iteritems()}
+        if type(obj) is list:
+            return [encode(i) for i in obj]
+        return obj
+    print hint
+    result = encode(result)
+    print '\n'.join(['  ' + i for i in pformat(result, width = 75).split('\n')])
+
+#importing API class
+from facepp import API
+api = API(API_KEY, API_SECRET)
+
+#assigning image with URL
+PERSONS = [
+    ('image1', 'http://trickster.site88.net/sou1.jpg'),
+    ('image2', 'http://trickster.site88.net/sou2.jpg'),
+]
+
+#assigning face ids
+
+result = api.detection.detect(url = 'http://trickster.site88.net/sou1.jpg', mode = 'oneface')
+face_idone = result['face'][0]['face_id']
+print(face_idone)
+
+result2 = api.detection.detect(url = 'http://trickster.site88.net/dad.JPG', mode = 'oneface')
+face_idtwo = result2['face'][0]['face_id']
+print(face_idtwo)
+
+
+#computing the similarity
+sim = api.recognition.compare(face_id1 = face_idone, face_id2 = face_idtwo)
+similarity = sim['similarity']
+print(similarity)
+
+
